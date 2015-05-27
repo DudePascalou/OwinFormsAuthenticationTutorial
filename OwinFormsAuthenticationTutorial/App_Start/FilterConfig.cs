7fe4f758fcbdf2e97831efbbb7c4855b80969768
Pascal Crouzet <dude.pascalou@gmail.com>
2015-05-27 21:45:52 +0200

    Sécurisation

--- OwinFormsAuthenticationTutorial/App_Start/FilterConfig.cs
+++ OwinFormsAuthenticationTutorial/App_Start/FilterConfig.cs
@@ -7,6 +7,7 @@ namespace OwinFormsAuthenticationTutorial
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
+            filters.Add(new AuthorizeAttribute());
             filters.Add(new HandleErrorAttribute());
         }
     }
